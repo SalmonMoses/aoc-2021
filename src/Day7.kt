5dc93a31f60049d844d8db058c004b068684ea37
Mikhail Melamed <mikhail.melamed@gmail.com>
2021-12-07 10:33:12 +0200

    Day 7

--- src/Day7.kt
+++ src/Day7.kt
@@ -1,17 +1,47 @@
+import java.lang.Math.abs
+
 class Day7 : AocDay<Long>() {
     override val name: String
         get() = "Day7"
     override val task1Spec: Long?
-        get() = null
+        get() = 37
     override val task2Spec: Long?
-        get() = null
+        get() = 168
 
     override fun task1(input: List<String>): Long {
-        TODO()
+        val positions = input[0].split(",").map { it.toInt() }
+        val minPosition = positions.minOf { it }
+        val maxPosition = positions.maxOf { it }
+        var minNeededFuel = Long.MAX_VALUE
+        findAlignment@ for (i in minPosition..maxPosition) {
+            var neededFuel = 0L
+            for (pos in positions) {
+                neededFuel += abs(pos - i)
+                if (neededFuel >= minNeededFuel) {
+                    continue@findAlignment
+                }
+            }
+            minNeededFuel = neededFuel
+        }
+        return minNeededFuel
     }
 
     override fun task2(input: List<String>): Long {
-        TODO()
+        val positions = input[0].split(",").map { it.toInt() }
+        val minPosition = positions.minOf { it }
+        val maxPosition = positions.maxOf { it }
+        var minNeededFuel = Long.MAX_VALUE
+        findAlignment@ for (i in minPosition..maxPosition) {
+            var neededFuel = 0L
+            for (pos in positions) {
+                neededFuel += (1..abs(pos - i)).sum()
+                if (neededFuel >= minNeededFuel) {
+                    continue@findAlignment
+                }
+            }
+            minNeededFuel = neededFuel
+        }
+        return minNeededFuel
     }
 }
 
---
+++ src/Day8.kt
@@ -0,0 +1,23 @@
+import java.lang.Math.abs
+
+class Day8 : AocDay<Long>() {
+    override val name: String
+        get() = "Day8"
+    override val task1Spec: Long?
+        get() = null
+    override val task2Spec: Long?
+        get() = null
+
+    override fun task1(input: List<String>): Long {
+        TODO()
+    }
+
+    override fun task2(input: List<String>): Long {
+        TODO()
+    }
+}
+
+fun main() {
+    val task = Day8()
+    task.run()
+}
