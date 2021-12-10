29d1b072bf702b35e3850b44c1b26194aae0aa18
Mikhail Melamed <mikhail.melamed@gmail.com>
2021-12-10 12:37:21 +0200

    Added template for day 11

--- src/AocDay.kt
+++ src/AocDay.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 abstract class AocDay<T> {
     abstract val name: String
     abstract val task1Spec: T?
@@ -6,18 +8,27 @@ abstract class AocDay<T> {
     abstract fun task2(input: List<String>): T
 
     fun readInput(): List<String> = readInput(name)
+    fun readTaskSpecInput(): List<String> = readInput("spec/$name.spec")
     fun readTask1SpecInput(): List<String> = readInput("spec/$name.spec1")
     fun readTask2SpecInput(): List<String> = readInput("spec/$name.spec2")
 
     fun run() {
         if (task1Spec != null) {
-            val spec1 = readTask1SpecInput()
-            val task1Actual = task1(spec1)
+            val spec = if (File("spec/$name.spec1").exists()) {
+                readTask1SpecInput()
+            } else {
+                readTaskSpecInput()
+            }
+            val task1Actual = task1(spec)
             check(task1Actual == task1Spec, { "Task 1 check failed with $task1Actual" })
         }
         if (task2Spec != null) {
-            val spec2 = readTask2SpecInput()
-            val task2Actual = task2(spec2)
+            val spec = if (File("spec/$name.spec2").exists()) {
+                readTask2SpecInput()
+            } else {
+                readTaskSpecInput()
+            }
+            val task2Actual = task2(spec)
             check(task2Actual == task2Spec, { "Task 2 check failed with $task2Actual" })
         }
         val input = readInput()
---
+++ src/Day11.kt
@@ -0,0 +1,21 @@
+class Day11 : AocDay<Long>() {
+    override val name: String
+        get() = "Day11"
+    override val task1Spec: Long?
+        get() = null
+    override val task2Spec: Long?
+        get() = null
+
+    override fun task1(input: List<String>): Long {
+        TODO()
+    }
+
+    override fun task2(input: List<String>): Long {
+        TODO()
+    }
+}
+
+fun main() {
+    val task = Day11()
+    task.run()
+}
