b501d7f6691ef0e59baf9c376cb1fc222a58018b
Mikhail Melamed <mikhail.melamed@gmail.com>
2021-12-11 20:10:38 +0200

    Day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,17 +1,94 @@
+class Octopus(var energy: Int) {
+    fun flash(): Boolean {
+        energy += 1
+        return energy == 10
+    }
+}
+
+class OctopusGrid(val grid: List<List<Octopus>>) {
+    fun flashAt(x: Int, y: Int): Long {
+        var flashes = 0L
+        if (grid[y][x].flash()) {
+            flashes += 1
+            (-1..1).forEach { dy ->
+                (-1..1).forEach { dx ->
+                    val newX = x + dx
+                    val newY = y + dy
+                    if (newX in (0..9) && newY in (0..9)) {
+                        flashes += flashAt(x + dx, y + dy)
+                    }
+                }
+            }
+        }
+        return flashes
+    }
+
+    fun reset() {
+        (0..9).forEach { y ->
+            (0..9).forEach { x ->
+                if (grid[y][x].energy >= 10) {
+                    grid[y][x].energy = 0
+                }
+            }
+        }
+    }
+}
+
 class Day11 : AocDay<Long>() {
     override val name: String
         get() = "Day11"
     override val task1Spec: Long?
-        get() = null
+        get() = 1656
     override val task2Spec: Long?
-        get() = null
+        get() = 195
 
     override fun task1(input: List<String>): Long {
-        TODO()
+        val octopuses = Array(10) { IntArray(10) { 0 } }
+        for (y in 0..9) {
+            for (x in 0..9) {
+                octopuses[y][x] = input[y][x].digitToInt()
+            }
+        }
+        val grid = OctopusGrid(octopuses.map { row ->
+            row.map { Octopus(it) }
+        })
+        var flashes = 0L
+        (0..99).forEach {
+            for (y in 0..9) {
+                for (x in 0..9) {
+                    flashes += grid.flashAt(x, y)
+                }
+            }
+            grid.reset()
+        }
+        return flashes
     }
 
     override fun task2(input: List<String>): Long {
-        TODO()
+        val octopuses = Array(10) { IntArray(10) { 0 } }
+        for (y in 0..9) {
+            for (x in 0..9) {
+                octopuses[y][x] = input[y][x].digitToInt()
+            }
+        }
+        val grid = OctopusGrid(octopuses.map { row ->
+            row.map { Octopus(it) }
+        })
+        var turns = 0L
+        while (true) {
+            turns++
+            var flashes = 0L
+            for (y in 0..9) {
+                for (x in 0..9) {
+                    flashes += grid.flashAt(x, y)
+                }
+            }
+            if (flashes == 100L) {
+                return turns
+            }
+            grid.reset()
+        }
+        return 0L
     }
 }
 
