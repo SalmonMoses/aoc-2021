64b23aabf309f898ddd9762140e071ca6ee88de3
Mikhail Melamed <mikhail.melamed@gmail.com>
2021-12-15 14:05:01 +0200

    Day 15 refactor

--- src/AocDay.kt
+++ src/AocDay.kt
@@ -7,10 +7,10 @@ abstract class AocDay<T> {
     abstract fun task1(input: List<String>): T
     abstract fun task2(input: List<String>): T
 
-    fun readInput(): List<String> = readInput(name)
-    fun readTaskSpecInput(): List<String> = readInput("spec/$name.spec")
-    fun readTask1SpecInput(): List<String> = readInput("spec/$name.spec1")
-    fun readTask2SpecInput(): List<String> = readInput("spec/$name.spec2")
+    private fun readInput(): List<String> = readInput(name)
+    private fun readTaskSpecInput(): List<String> = readInput("spec/$name.spec")
+    private fun readTask1SpecInput(): List<String> = readInput("spec/$name.spec1")
+    private fun readTask2SpecInput(): List<String> = readInput("spec/$name.spec2")
 
     fun run() {
         if (task1Spec != null) {
@@ -19,8 +19,10 @@ abstract class AocDay<T> {
             } else {
                 readTaskSpecInput()
             }
+            val time = System.currentTimeMillis()
             val task1Actual = task1(spec)
-            check(task1Actual == task1Spec, { "Task 1 check failed with $task1Actual" })
+            val failed = task1Actual != task1Spec
+            println("Task 1 Spec = $task1Actual ${if(failed) "!= $task1Spec - Failed!" else "== $task1Spec"} [${System.currentTimeMillis() - time} ms]")
         }
         if (task2Spec != null) {
             val spec = if (File("spec/$name.spec2.txt").exists()) {
@@ -28,12 +30,18 @@ abstract class AocDay<T> {
             } else {
                 readTaskSpecInput()
             }
+            val time = System.currentTimeMillis()
             val task2Actual = task2(spec)
-            check(task2Actual == task2Spec, { "Task 2 check failed with $task2Actual" })
+            val failed = task2Actual != task2Spec
+            println("Task 2 Spec = $task2Actual ${if(failed) "!= $task2Spec - Failed!" else "== $task2Spec"} [${System.currentTimeMillis() - time} ms]")
         }
         val input = readInput()
         check(input.isNotEmpty(), { "Input is empty!" })
-        println(task1(input))
-        println(task2(input))
+        var time = System.currentTimeMillis()
+        val task1Solution = task1(input)
+        println("Task 1 Solution = $task1Solution [${System.currentTimeMillis() - time} ms]")
+        time = System.currentTimeMillis()
+        val task2Solution = task2(input)
+        println("Task 2 Solution = $task2Solution [${System.currentTimeMillis() - time} ms]")
     }
 }
--- src/Day15.kt
+++ src/Day15.kt
@@ -8,29 +8,24 @@ class Day15 : AocDay<Long>() {
     override val task2Spec: Long?
         get() = 315
 
-    fun manhattan(from: GridPoint, to: GridPoint): Int {
-        return Math.abs(from.first - to.first) + Math.abs(from.second - to.second)
-    }
-
-    fun aStar(grid: Grid<Int>, from: GridPoint, to: GridPoint): Long {
+    fun dijkstra(grid: Grid<Int>, from: GridPoint, to: GridPoint): Long {
         val frontier = PriorityQueue<Pair<GridPoint, Int>>(Comparator.comparingInt { it.second })
         val costs = mutableMapOf<GridPoint, Int>()
 
         frontier.add(Pair(from, 0))
         costs.set(from, 0)
         while (!frontier.isEmpty()) {
-            val current = frontier.poll()
+            val current = frontier.poll().first
 
-            if (current.first == to) {
+            if (current == to) {
                 break
             }
 
-            grid.getNeighbors(current.first.first, current.first.second).forEach { next ->
-                val newCost = costs[current.first]!! + grid[next.first, next.second]
+            grid.getNeighbors(current.first, current.second).forEach { next ->
+                val newCost = costs[current]!! + grid[next.first, next.second]
                 if (next !in costs || newCost < costs[next]!!) {
                     costs.set(next, newCost)
-                    val priority = newCost + manhattan(next, to)
-                    frontier.add(Pair(next, priority))
+                    frontier.add(Pair(next, newCost))
                 }
             }
         }
@@ -54,12 +49,12 @@ class Day15 : AocDay<Long>() {
 
     fun repeatMap(original: Grid<Int>, times: Int): Grid<Int> {
         val originalLists = original.grid.map { it.toMutableList() }.toMutableList()
-        for (i in 1..4) {
+        for (i in 1 until times) {
             originalLists.addAll(incrementGrid(original, i).grid.map { row -> row.toMutableList() })
         }
         for (row in originalLists) {
             val originalRow = row.toList()
-            for(i in 1..4) {
+            for (i in 1 until times) {
                 row.addAll(incrementRow(originalRow, i))
             }
         }
@@ -68,12 +63,12 @@ class Day15 : AocDay<Long>() {
 
     override fun task1(input: List<String>): Long {
         val grid = Grid(input.map { row -> row.map { it.digitToInt() } })
-        return aStar(grid, 0 to 0, grid.width - 1 to grid.height - 1)
+        return dijkstra(grid, 0 to 0, grid.width - 1 to grid.height - 1)
     }
 
     override fun task2(input: List<String>): Long {
         val grid = Grid(input.map { row -> row.map { it.digitToInt() } })
-        return aStar(repeatMap(grid, 5), 0 to 0, grid.width * 5 - 1 to grid.height * 5 - 1)
+        return dijkstra(repeatMap(grid, 5), 0 to 0, grid.width * 5 - 1 to grid.height * 5 - 1)
     }
 }
 
